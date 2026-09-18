97581ccd285e7441b995cd2496c1c8dfb851dae1
Arthur Yuji Mendes Suzuki <yujisaccont@gmail.com>
2026-09-18 15:49:52 -0300

    funcionalidade extra

--- gerenciador-projetos/src/main/java/br/unisales/gerenciador_projetos/repository/TarefaRepository.java
+++ gerenciador-projetos/src/main/java/br/unisales/gerenciador_projetos/repository/TarefaRepository.java
@@ -4,4 +4,7 @@ import br.unisales.gerenciadorprojetos.entity.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    List<Tarefa> findByStatus(String status);
+
 }
