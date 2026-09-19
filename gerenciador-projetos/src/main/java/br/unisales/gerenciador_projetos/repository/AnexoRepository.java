64053750b65f47b2073676fb568ffecebf99ea41
Luiz <lamasluizmiguel@gmail.com>
2026-09-19 14:10:21 -0300

    Importei o List no anexo reposi
     List<Anexo> findByTarefaIdTarefa(Long idTarefa);

--- gerenciador-projetos/src/main/java/br/unisales/gerenciador_projetos/repository/AnexoRepository.java
+++ gerenciador-projetos/src/main/java/br/unisales/gerenciador_projetos/repository/AnexoRepository.java
@@ -2,6 +2,8 @@ package br.unisales.gerenciador_projetos.repository;
 
 import br.unisales.gerenciador_projetos.entity.Anexo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface AnexoRepository extends JpaRepository<Anexo, Long> {
+     List<Anexo> findByTarefaIdTarefa(Long idTarefa);
 }
